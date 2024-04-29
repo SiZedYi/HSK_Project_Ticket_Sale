daf4afedabb1f137a141d29a525d9d7b905999b2
nguyenngochuen <87378600+nguyenngochuen@users.noreply.github.com>
2024-04-29 21:40:08 +0700

    Update LoaiPhongChieu.java

--- Ticket_Sale/src/enums/LoaiPhongChieu.java
+++ Ticket_Sale/src/enums/LoaiPhongChieu.java
@@ -1,5 +1,18 @@
 package enums;
 
 public enum LoaiPhongChieu {
+  	BASIC("Cơ bản"), P4D("4D"), PREMIUM("Premium Cinema");
+	
+	
+	private final String displayname;
+	
+	private LoaiPhongChieu (String displayname) {
+		this.displayname= displayname;
+	}
+	
+	@Override
+	public String toString() {
+		return displayname;
+	}
 
 }
