35b12a3b3dc5ab38fcd1134f6649af2afe35e7c8
nguyenngochuen <87378600+nguyenngochuen@users.noreply.github.com>
2024-04-30 00:38:54 +0700

    Update TheLoai.java

--- Ticket_Sale/src/enums/TheLoai.java
+++ Ticket_Sale/src/enums/TheLoai.java
@@ -1,5 +1,25 @@
 package enums;
 
 public enum TheLoai {
+  	HANH_DONG("Hành động"), 
+	TINH_CAM("Tình cảm"),
+	HAI("Hài huớc"),
+	LANG_MAN("Lãng mạn"),
+	HOAT_HINNH("Hoạt hình"),
+	HINH_SU("Hình sự"),
+	TAI_LIEU("Tài liệu");
+	
+	
+	private final String displayname;
+	
+	private TheLoai(String displayname) {
+		this.displayname= displayname;
+	}
+	
+	@Override
+	public String toString() {
+		return displayname;
+	}
+
 
 }
