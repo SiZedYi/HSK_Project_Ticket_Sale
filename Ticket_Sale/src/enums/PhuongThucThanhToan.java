11e1255e08b34dd63b758b9e38ea5bbeaac9de20
nguyenngochuen <87378600+nguyenngochuen@users.noreply.github.com>
2024-04-29 21:45:28 +0700

    Update PhuongThucThanhToan.java

--- Ticket_Sale/src/enums/PhuongThucThanhToan.java
+++ Ticket_Sale/src/enums/PhuongThucThanhToan.java
@@ -1,5 +1,24 @@
 package enums;
 
 public enum PhuongThucThanhToan {
+  	CASH("Tiền mặt"),
+	MOMO("MOMO"),
+	ATM("Thẻ ngân hàng"),
+	VNPAY("VNPAY"),
+	ZALOPAY("Zalo Pay"),
+	BANKTRANSFER("Chuyển khoản qua ngân hàng");
+
+	
+
+	private final String displayname;
+	
+	private PhuongThucThanhToan(String displayname) {
+		this.displayname= displayname;
+	}
+	
+	@Override
+	public String toString() {
+		return displayname;
+	}
 
 }
