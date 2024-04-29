6983dd5d1d0cc91e159523bc8162cbdc8e6d0184
nguyenngochuen <87378600+nguyenngochuen@users.noreply.github.com>
2024-04-29 21:32:47 +0700

    Update GioiHanTuoi.java

--- Ticket_Sale/src/enums/GioiHanTuoi.java
+++ Ticket_Sale/src/enums/GioiHanTuoi.java
@@ -1,5 +1,25 @@
 package enums;
 
 public enum GioiHanTuoi {
+  P("Mọi độ tuổi"),
+	K("Dưới 13 tuổi xem cùng cha,mẹ hoặc người giám hộ"),
+	T13("Cấm khán giả dưới 13 tuổi"),
+	T16("Cấm khán giả dưới 16 tuổi"),
+	T18("Cấm khán giả dưới 18 tuổi");
+	
+	
+	
+	
+	private final String displayname;
+	
+	private GioiHanTuoi (String displayname) {
+		this.displayname= displayname;
+	}
+	
+	@Override
+	public String toString() {
+		return displayname;
+	}
+
 
 }
