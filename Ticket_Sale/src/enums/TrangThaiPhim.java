2637404b8937d2d3a37571078eaeb9e00eff83ea
nguyenngochuen <87378600+nguyenngochuen@users.noreply.github.com>
2024-04-30 00:29:57 +0700

    Update TrangThaiPhim.java

--- Ticket_Sale/src/enums/TrangThaiPhim.java
+++ Ticket_Sale/src/enums/TrangThaiPhim.java
@@ -1,5 +1,18 @@
 package enums;
 
 public enum TrangThaiPhim {
+  	SAPCHIEU("Sắp chiếu"),DANGCHIEU("Đang chiếu"); 
+	
+	private final String displayname;
+	
+	private  TrangThaiPhim(String displayname) {
+		this.displayname= displayname;
+	}
+	
+	@Override
+	public String toString() {
+		return displayname;
+	}
+
 
 }
