b632c1d51b74310f4af9694de5f75d5dc0985b0e
nguyenngochuen <87378600+nguyenngochuen@users.noreply.github.com>
2024-04-29 23:58:26 +0700

    Update Quyen.java

--- Ticket_Sale/src/enums/Quyen.java
+++ Ticket_Sale/src/enums/Quyen.java
@@ -1,5 +1,17 @@
 package enums;
 
 public enum Quyen {
+  NHANVIEN("Nhân viên"), QUANlY("Quản lý");
+  private final String displayname;
+	private Quyen(String displayname) {
+		this.displayname = displayname;
+		// TODO Auto-generated constructor stub
+	}
+
+
+	@Override
+	public String toString() {
+		return displayname ;
+	}
 
 }
