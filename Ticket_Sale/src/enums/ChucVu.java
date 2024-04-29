82bd6b4c1f52a579e4c8708885e117f986caece9
nguyenngochuen <87378600+nguyenngochuen@users.noreply.github.com>
2024-04-29 23:57:56 +0700

    Update ChucVu.java

--- Ticket_Sale/src/enums/ChucVu.java
+++ Ticket_Sale/src/enums/ChucVu.java
@@ -1,18 +1,5 @@
 package enums;
 
 public enum ChucVu {
-  NHANVIEN("Nhân viên"), QUANlY("Quản lý");
-  private final String displayname;
-	private ChucVu(String displayname) {
-		this.displayname = displayname;
-		// TODO Auto-generated constructor stub
-	}
-
-
-	@Override
-	public String toString() {
-		return displayname ;
-	}
-	
 
 }
