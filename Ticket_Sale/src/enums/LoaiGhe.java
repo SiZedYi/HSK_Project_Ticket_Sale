83eb24362f4285c64e6f7425f39d9be279b4a062
nguyenngochuen <87378600+nguyenngochuen@users.noreply.github.com>
2024-04-29 21:26:04 +0700

    Update LoaiGhe.java

--- Ticket_Sale/src/enums/LoaiGhe.java
+++ Ticket_Sale/src/enums/LoaiGhe.java
@@ -1,5 +1,19 @@
 package enums;
 
 public enum LoaiGhe {
+  GHETIEUCHUAN("Ghế tiêu chuẩn"), GHEVIP("Ghế VIP"),GHECOUPLE("Ghế COUPLE");
+	
+	
+	
+	private final String displayname;
+	
+	private LoaiGhe (String displayname) {
+		this.displayname= displayname;
+	}
+	
+	@Override
+	public String toString() {
+		return displayname;
+	}
 
 }
