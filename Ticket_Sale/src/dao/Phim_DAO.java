45edf9aae106168aac646f428a134c379477e57e
SiZedYi <91822460+SiZedYi@users.noreply.github.com>
2024-05-06 02:03:06 +0700

    add CRUD func

--- Ticket_Sale/src/dao/Phim_DAO.java
+++ Ticket_Sale/src/dao/Phim_DAO.java
@@ -1,5 +1,6 @@
 package dao;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -21,7 +22,7 @@ public class Phim_DAO extends Dao<Phim>{
 	@Override
 	public Phim readRow(ResultSet rs) {
 		Phim phim = null;
-		
+
 		try {
 			// TODO Auto-generated method stub
 			ArrayList<TheLoai> listTheLoai = new ArrayList<TheLoai>();
@@ -36,12 +37,203 @@ public class Phim_DAO extends Dao<Phim>{
 		return phim;
 	}
 
+	
+	public boolean create(Phim phim) {
+		String trangThaiValue ="SAP_CHIEU";
+		String theLoaiValue = "HANH_DONG";
+		String gioiHanTuoiValue = "P";
+		ConnectDB.getInstance();
+		Connection con = ConnectDB.getConnection();
+		PreparedStatement statement = null;
+		int n=0;
+		
+		// Xử lý enums trạng thái
+	    if (phim.getTrangThai().toString().equals("Đang chiếu"))
+	        trangThaiValue = "DANG_CHIEU";
+
+	    // Xử lý enums thể loại
+	    String selectedTheLoai = phim.getTheLoai().toString();
+	    switch (selectedTheLoai) {
+	        case "Hành động":
+	            theLoaiValue = "HANH_DONG";
+	            break;
+	        case "Tình cảm":
+	            theLoaiValue = "TINH_CAM";
+	            break;
+	        case "Hài hước":
+	            theLoaiValue = "HAI_HUOC";
+	            break;
+	        case "Lãng mạn":
+	            theLoaiValue = "LANG_MAN";
+	            break;
+	        case "Hoạt hình":
+	            theLoaiValue = "HOAT_HINH";
+	            break;
+	        case "Hình sự":
+	            theLoaiValue = "HINH_SU";
+	            break;
+	        case "Tài liệu":
+	            theLoaiValue = "TAI_LIEU";
+	            break;
+	    }
+
+		
+		// Xử lý enum giới hạn tuổi
+	    String selectedGhTuoi = phim.getGioiHanTuoi().toString();
+	    switch (selectedGhTuoi) {
+	        case "Mọi độ tuổi":
+	            gioiHanTuoiValue = "P";
+	            break;
+	        case "Dưới 13 tuổi xem cùng cha,mẹ hoặc người giám hộ":
+	            gioiHanTuoiValue = "K";
+	            break;
+	        case "Cấm khán giả dưới 13 tuổi":
+	            gioiHanTuoiValue = "T13";
+	            break;
+	        case "Cấm khán giả dưới 16 tuổi":
+	            gioiHanTuoiValue = "T16";
+	            break;
+	        case "Cấm khán giả dưới 18 tuổi":
+	            gioiHanTuoiValue = "T18";
+	            break;
+	    }
+		try {
+			statement= con.prepareStatement("insert into" + 
+		" Phim values(?,?,?,?,?,?,?)");
+			statement.setString(1, phim.getMaPhim());
+			statement.setString(2, phim.getTenPhim());
+			statement.setString(3, trangThaiValue);
+			statement.setDouble(4, phim.getGia());
+			statement.setString(5, theLoaiValue);
+			statement.setString(6, gioiHanTuoiValue);
+			statement.setString(7, phim.getAnhPhim());
+			n =statement.executeUpdate();	
+			
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}finally {
+			try {
+				statement.close();
+			}catch (SQLException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		return n>0;
+	}
+	
+	public boolean update(Phim phim) {
+	    String trangThaiValue = "SAP_CHIEU";
+	    String theLoaiValue = "HANH_DONG";
+	    String gioiHanTuoiValue = "P";
+	    ConnectDB.getInstance();
+	    Connection con = ConnectDB.getConnection();
+	    PreparedStatement statement = null;
+	    int n = 0;
+
+	    // Xử lý enums trạng thái
+	    if (phim.getTrangThai().toString().equals("Đang chiếu"))
+	        trangThaiValue = "DANG_CHIEU";
+
+	    // Xử lý enums thể loại
+	    String selectedTheLoai = phim.getTheLoai().toString();
+	    switch (selectedTheLoai) {
+	        case "Hành động":
+	            theLoaiValue = "HANH_DONG";
+	            break;
+	        case "Tình cảm":
+	            theLoaiValue = "TINH_CAM";
+	            break;
+	        case "Hài hước":
+	            theLoaiValue = "HAI_HUOC";
+	            break;
+	        case "Lãng mạn":
+	            theLoaiValue = "LANG_MAN";
+	            break;
+	        case "Hoạt hình":
+	            theLoaiValue = "HOAT_HINH";
+	            break;
+	        case "Hình sự":
+	            theLoaiValue = "HINH_SU";
+	            break;
+	        case "Tài liệu":
+	            theLoaiValue = "TAI_LIEU";
+	            break;
+	    }
+
+	    // Xử lý enum giới hạn tuổi
+	    String selectedGhTuoi = phim.getGioiHanTuoi().toString();
+	    switch (selectedGhTuoi) {
+	        case "Mọi độ tuổi":
+	            gioiHanTuoiValue = "P";
+	            break;
+	        case "Dưới 13 tuổi xem cùng cha,mẹ hoặc người giám hộ":
+	            gioiHanTuoiValue = "K";
+	            break;
+	        case "Cấm khán giả dưới 13 tuổi":
+	            gioiHanTuoiValue = "T13";
+	            break;
+	        case "Cấm khán giả dưới 16 tuổi":
+	            gioiHanTuoiValue = "T16";
+	            break;
+	        case "Cấm khán giả dưới 18 tuổi":
+	            gioiHanTuoiValue = "T18";
+	            break;
+	    }
+
+	    try {
+	        statement = con.prepareStatement("UPDATE Phim " +
+	                "SET tenPhim = ?, trangThai = ?, gia = ?, theLoai = ?, gioiHanTuoi = ?, hinhAnh = ? " +
+	                "WHERE MaPhim = ?");
+	        statement.setString(1, phim.getTenPhim());
+	        statement.setString(2, trangThaiValue);
+	        statement.setDouble(3, phim.getGia());
+	        statement.setString(4, theLoaiValue);
+	        statement.setString(5, gioiHanTuoiValue);
+	        statement.setString(6, phim.getAnhPhim());
+	        statement.setString(7, phim.getMaPhim());
+	        n = statement.executeUpdate();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } finally {
+	        try {
+	            statement.close();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    return n > 0;
+	}
+	
+	public boolean delete(String maPhim) {
+	    ConnectDB.getInstance();
+	    Connection con = ConnectDB.getConnection();
+	    PreparedStatement statement = null;
+	    int n = 0;
+
+	    try {
+	        statement = con.prepareStatement("DELETE FROM Phim WHERE maPhim = ?");
+	        statement.setString(1, maPhim);
+	        n = statement.executeUpdate();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } finally {
+	        try {
+	            statement.close();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    return n > 0;
+	}
+
 	@Override
 	public void insertingStatement(PreparedStatement stmt, Phim t) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
 	@Override
 	public void updatingStatement(PreparedStatement stmt, Phim t) {
 		// TODO Auto-generated method stub
